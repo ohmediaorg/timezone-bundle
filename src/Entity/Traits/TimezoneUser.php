61acb9d793c8bba52877e832de0d91e38e72d582
Justin Hoffman <justin@ohmedia.ca>
2022-03-30 11:48:24 -0600

    create the timezone user trait

---
+++ src/Entity/Traits/TimezoneUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OHMedia\TimezoneBundle\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait TimezoneUser
+{
+    #[ORM\Column(type: 'string', length: 64, nullable: true)]
+    protected $timezone;
+
+    public function getTimezone(): string
+    {
+        return (string) $this->timezone;
+    }
+
+    public function setTimezone(?string $timezone): self
+    {
+        $this->timezone = $timezone;
+
+        return $this;
+    }
+}
