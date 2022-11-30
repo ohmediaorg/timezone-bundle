1efba2c96d4f640e365a70d3ac0c5942c1ce7ed3
Justin Hoffman <justin@ohmedia.ca>
2022-11-30 14:17:51 -0600

    move timezone get/set into a separate service

--- src/EventListener/TimezoneListener.php
+++ src/EventListener/TimezoneListener.php
@@ -2,52 +2,20 @@
 
 namespace OHMedia\TimezoneBundle\EventListener;
 
-use OHMedia\TimezoneBundle\Traits\TimezoneUser;
+use OHMedia\TimezoneBundle\Service\Timezone;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class TimezoneListener
 {
-    private $tokenStorage;
-    private $defaultTimezone;
+    private $timezone;
 
-    public function __construct(TokenStorageInterface $tokenStorage, $defaultTimezone)
+    public function __construct(Timezone $timezone)
     {
-        $this->tokenStorage = $tokenStorage;
-        $this->defaultTimezone = $defaultTimezone;
+        $this->timezone = $timezone;
     }
 
     public function onKernelRequest(RequestEvent $event)
     {
-        $token = $this->tokenStorage->getToken();
-
-        $user = $token ? $token->getUser() : null;
-
-        $timezone = null;
-
-        if ($user && !in_array(TimezoneUser::class, $this->getTraits($user))) {
-            $timezone = $user->getTimezone();
-        }
-
-        if (empty($timezone)) {
-            $timezone = $this->defaultTimezone;
-        }
-
-        date_default_timezone_set($timezone);
-    }
-
-    private function getTraits($class)
-    {
-        $traits = [];
-
-        do {
-            $traits = array_merge(class_uses($class), $traits);
-        } while($class = get_parent_class($class));
-
-        foreach ($traits as $trait => $same) {
-            $traits = array_merge(class_uses($trait), $traits);
-        }
-
-        return array_unique($traits);
+        $this->timezone->set();
     }
 }
---
+++ src/Service/Timezone.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace OHMedia\TimezoneBundle\Service;
+
+use OHMedia\TimezoneBundle\Traits\TimezoneUser;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class Timezone
+{
+    private $tokenStorage;
+    private $defaultTimezone;
+
+    public function __construct(TokenStorageInterface $tokenStorage, $defaultTimezone)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->defaultTimezone = $defaultTimezone;
+    }
+
+    public function set(): self
+    {
+        $token = $this->tokenStorage->getToken();
+
+        $user = $token ? $token->getUser() : null;
+
+        $timezone = null;
+
+        if ($user && !in_array(TimezoneUser::class, $this->getTraits($user))) {
+            $timezone = $user->getTimezone();
+        }
+
+        if (empty($timezone)) {
+            $timezone = $this->defaultTimezone;
+        }
+
+        date_default_timezone_set($timezone);
+
+        return $this;
+    }
+
+    public function get(): string
+    {
+        $this->set();
+
+        return date_default_timezone_get();
+    }
+
+    private function getTraits($class): array
+    {
+        $traits = [];
+
+        do {
+            $traits = array_merge(class_uses($class), $traits);
+        } while($class = get_parent_class($class));
+
+        foreach ($traits as $trait => $same) {
+            $traits = array_merge(class_uses($trait), $traits);
+        }
+
+        return array_unique($traits);
+    }
+}
