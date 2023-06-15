2048a026366d4317426c5de9cb0b61d166617df5
Justin Hoffman <justin@ohmedia.ca>
2023-06-15 16:36:21 -0600

    remove unneeded class include

--- src/DependencyInjection/OHMediaTimezoneExtension.php
+++ src/DependencyInjection/OHMediaTimezoneExtension.php
@@ -2,7 +2,6 @@
 
 namespace OHMedia\TimezoneBundle\DependencyInjection;
 
-use OHMedia\TimezoneBundle\Provider\AbstractEntityProvider;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
