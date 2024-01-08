9d19643cd0ee93b0e049182550aacf0da2db6df8
justin-oh <justin@ohmedia.ca>
2024-01-08 15:40:40 -0600

    deprecation fix

--- src/Doctrine/Dbal/UTCDateTimeImmutableType.php
+++ src/Doctrine/Dbal/UTCDateTimeImmutableType.php
@@ -2,10 +2,65 @@
 
 namespace OHMedia\TimezoneBundle\Doctrine\Dbal;
 
-class UTCDateTimeImmutableType extends AbstractUTCDateTimeType
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeImmutableType;
+
+/**
+ * Type that maps an SQL DATETIME/TIMESTAMP to a PHP DateTimeImmutable object.
+ */
+class UTCDateTimeImmutableType extends DateTimeImmutableType
 {
-    protected function convertValue($formatString, $value, \DateTimeZone $tz)
+    /**
+     * @var \DateTimeZone
+     */
+    private static $utc;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
     {
-        return \DateTimeImmutable::createFromFormat($formatString, $value, $tz);
+        if ($value instanceof \DateTimeImmutable) {
+            $value = $value->setTimezone(self::getUtc());
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): mixed
+    {
+        if ($value === null || $value instanceof \DateTimeInterface) {
+            return $value;
+        }
+
+        $dateTime = \DateTimeImmutable::createFromFormat(
+            $platform->getDateTimeFormatString(),
+            $value,
+            self::getUtc()
+        );
+
+        if ($dateTime !== false) {
+            return $dateTime;
+        }
+
+        try {
+            return new \DateTimeImmutable($value, self::getUtc());
+        } catch (\Exception $e) {
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeFormatString(),
+                $e,
+            );
+        }
+    }
+
+    private static function getUtc(): \DateTimeZone
+    {
+        return self::$utc ?: self::$utc = new \DateTimeZone('UTC');
     }
 }
